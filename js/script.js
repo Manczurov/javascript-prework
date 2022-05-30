a3b1053abe6fc4c7a941fc7cda34c4698cb9a8a2
Michał Chmurski <chmurski.michal@gmail.com>
2022-05-30 22:37:12 +0200

    update script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-var playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '2') {
+    computerMove = 'papier';
+  } else {
+    computerMove = 'nieznany ruch';
+  }
+  if (randomNumber == '3') {
+    computerMove = 'nożyce';
+  } else {
+    computerMove = 'nieznany ruch';
+  }
+printMessage('Mój ruch: ' + computerMove);
